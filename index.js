f0b2a38be1990253203ebb72a0178d24b3d458b8
NestorCalzadilla <00110622@uca.edu.sv>
2024-05-07 13:53:20 -0600

    Add: Guardar a DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/db.js";
+import Device from "./models/Device.js";
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,8 +16,26 @@ try {
     console.log(error);
 }
 
-app.post("/uplinks", (req, res) => {
+app.post("/uplinks", async (req, res) => {
     console.log(req.body.uplink_message.decoded_payload.str)
+    console.log(req.body.identifiers.device_ids.device_id)
+    const value = req.body.uplink_message.decoded_payload.str
+    const deviceId = req.body.identifiers.device_ids.device_id
+    try {
+        let device = await Device.findByPk(deviceId)
+        if(!device){
+            await Device.create({
+                id: deviceId,
+                value: [value]
+            })
+        }
+        else{
+            device.value = [value]
+            device.save()
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.listen(port, () => {
