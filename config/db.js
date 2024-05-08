c5499e2f795dc7484eb5fd2fd024e396b2bd897b
Ddennys <54415294+DennysRivera@users.noreply.github.com>
2024-05-08 00:41:56 -0600

    Debugging

--- config/db.js
+++ config/db.js
@@ -5,13 +5,13 @@ dotenv.config();
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
+    logging: false
+    /*dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
-    }
+    }*/
 });
 
 export default db;
