c5698f64319b9e01bf435aaa18e2c064815dd7e7
DennysRivera <00026919@uca.edu.sv>
2024-07-11 14:37:24 -0600

    Logger con mensajes en consola

--- tesis_dashboard/index.js
+++ tesis_dashboard/index.js
@@ -30,11 +30,13 @@ const port = process.env.PORT || 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(morgan("json"));
+// Logger con mensajes en consola
+app.use(morgan(morganFormato));
 
-app.use(morgan(morganFormato, {
+// Logger con mensajes en archivo
+/*app.use(morgan(morganFormato, {
     stream: fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" })
-}));
+}));*/
 
 app.use(helmet());
 
