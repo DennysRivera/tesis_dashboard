a065d47a1c7589f0ce102bcc0e6edc3f6be5d76a
DennysRivera <00026919@uca.edu.sv>
2024-08-13 14:10:25 -0600

    Modify: CORS methods

--- tesis_dashboard/utils/morganFormato.js
+++ tesis_dashboard/utils/morganFormato.js
@@ -6,6 +6,7 @@ export function morganFormato(tokens, req, res){
         'HTTP/' + tokens['http-version'](req, res) + '\"',
         'Status: ' + tokens.status(req, res),
         'Message: ' + (res.statusCode >= 400 ? res.statusMessage : '-'),
-        tokens['user-agent'](req, res)
+        tokens['user-agent'](req, res),
+        "Referer: " + tokens.referrer(req, res)
     ].join(', ');
 }
